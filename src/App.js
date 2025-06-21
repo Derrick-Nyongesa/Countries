ccecaafbf060b13bf601a788ee7023f533513d45
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2025-06-21 11:02:19 +0300

    set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Box, TextField } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <></>
+    // <Box
+    //   component="form"
+    //   sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
+    //   noValidate
+    //   autoComplete="off"
+    // >
+    //   <TextField id="outlined-basic" variant="outlined" />
+    // </Box>
   );
 }
 
