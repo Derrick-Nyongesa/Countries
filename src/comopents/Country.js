04792310490ae5865fe68c60510c71ebf6271257
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2025-06-25 19:31:31 +0300

    home button

--- src/comopents/Country.js
+++ src/comopents/Country.js
@@ -92,6 +92,16 @@ function Country() {
           }}
         />
       </Box>
+      <Box px={3} py={2}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => (window.location.href = "/")}
+          startIcon={<MenuIcon />}
+        >
+          Back to Home
+        </Button>
+      </Box>
 
       <Box display="flex" alignItems="center" gap={2} px={3} py={2}>
         <Avatar
